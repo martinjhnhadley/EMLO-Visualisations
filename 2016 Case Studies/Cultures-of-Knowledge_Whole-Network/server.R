8ca89c760223350ee6c5affb8be1535dd4d36d1a
Martin Hadley <martinjohnhadley@gmail.com>
2016-04-12 16:48:02 +0100

    German Letters: Created new choropleth visualisation

--- 2016 Case Studies/Cultures-of-Knowledge_Whole-Network/server.R
+++ 2016 Case Studies/Cultures-of-Knowledge_Whole-Network/server.R
@@ -1,3 +1,13 @@
+## =============================== License ========================================
+## ================================================================================
+## This work is distributed under the MIT license, included in the parent directory
+## Copyright Owner: University of Oxford
+## Date of Authorship: 2016
+## Author: Martin John Hadley (orcid.org/0000-0002-3039-6849)
+## Academic Contact: Arno Bosse (http://orcid.org/0000-0003-3681-1289)
+## Data Source: emlo.bodleian.ox.ac.uk
+## ================================================================================
+
 ## ==== Packages to load for server
 
 library(shiny) # Some advanced functionality depends on the shiny package being loaded server-side, including plot.ly
