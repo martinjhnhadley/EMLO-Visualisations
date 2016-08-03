60b4742559acea74d427426b5e0987e0ba64122b
martinjhnhadley <martinjohnhadley@gmail.com>
2016-08-03 11:55:17 +0100

    Added lines between segements

--- 2016/Mireia Borrell/server.R
+++ 2016/Mireia Borrell/server.R
@@ -8,6 +8,25 @@ library(shinyBS)
 
 source("data-processing.R", local = T)
 
+gantt_labeler <-
+  function(start_date = NA,
+           end_date = NA,
+           y_axis = NA,
+           color = NA) {
+    paste0(
+      "Policy Name: ",
+      y_axis,
+      "</br>",
+      "Enforcement Period: ",
+      start_date,
+      " to ",
+      end_date,
+      "</br>",
+      "Policy Type: ",
+      color
+    )
+  }
+
 shinyServer(function(input, output, session) {
   output$timeline_timline_selected_cols_UI <- renderUI({
     selectInput(
@@ -45,11 +64,30 @@ shinyServer(function(input, output, session) {
         xend = Valid.until..c...,
         y = Name.Policy,
         yend = Name.Policy,
-        colour = Type.of.policy
+        colour = Type.of.policy,
+        text = gantt_labeler(
+          start_date = Valid.from..b.,
+          end_date = Valid.until..c...,
+          y_axis = Name.Policy,
+          color = Type.of.policy
+        )
       )
     ) +
-      geom_segment(size = 3) +
-      xlab("Date") + ylab(""))
+      geom_segment(size = 4) +
+      geom_segment(
+        aes(
+          x = Valid.from..b. - 25,
+          xend = Valid.from..b.,
+          y = Name.Policy,
+          yend = Name.Policy
+        ),
+        colour = "black",
+        size = 4,
+        arrow=arrow(type = "closed")
+      ) +
+      # geom_segment(data=timeline_data, aes(x=Valid.from..b., xend=Valid.until..c..., y=Name.Policy, yend=Name.Policy), size=11, colour="red") + 
+      # geom_segment(data=timeline_data, aes(x=Valid.from..b. + 0.1, xend=Valid.until..c... - 0.1, y=Name.Policy, yend=Name.Policy), size=11, colour="red") +
+      xlab("Date") + ylab(""), tooltip = "text")
     
   })
   
