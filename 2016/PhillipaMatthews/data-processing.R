e84b75f4935a8af9f1adf3fc178ed22257bc11e0
martinjhnhadley <martinjohnhadley@gmail.com>
2016-08-03 17:34:24 +0100

    Added basic heliotopes datatable

---
+++ 2016/PhillipaMatthews/data-processing.R
@@ -0,0 +1,5 @@
+heliotope_df <- read.csv("https://docs.google.com/spreadsheets/d/18-QtuiOHTOpqEixqgyGG4GUizu3xFtUnPZEkIOJhBOA/pub?gid=1871412950&single=true&output=csv", stringsAsFactors = F)
+
+
+
+str(heliotope_df)
---
+++ 2016/PhillipaMatthews/server.R
@@ -0,0 +1,33 @@
+library(DT)
+library(shiny)
+
+heliotope_df <- read.csv("https://docs.google.com/spreadsheets/d/18-QtuiOHTOpqEixqgyGG4GUizu3xFtUnPZEkIOJhBOA/pub?gid=1871412950&single=true&output=csv", stringsAsFactors = F)
+
+colnames(heliotope_df) <- gsub("[.]", " ", colnames(heliotope_df))
+
+heliotope_df$`Publication year` <- as.numeric(heliotope_df$`Publication year`)
+
+initial_columns <- c("Record number", "HBV protein", "Sequence numbers within protein  based on reference sequence ", "Database", "Authors", "Title", "Journal", 
+                     "Publication year","DOI")
+
+shinyServer(
+  function(input, output){
+    
+    output$heliotopes_selected_cols_UI <- renderUI({
+      selectInput(
+        "heliotopes_selected_cols",
+        label = "Columns to show: ",
+        choices = colnames(heliotope_df),
+        selected = initial_columns,
+        multiple = TRUE,
+        width = "100%"
+      )
+    })
+    
+    output$heliotopes_datatable <- DT::renderDataTable(
+      heliotope_df[,input$heliotopes_selected_cols],
+      rownames = FALSE, filter = 'top', escape = FALSE
+    )
+    
+  }
+)
