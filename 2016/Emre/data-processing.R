d017f933aa0fe10d183adde4d5168a10f0a2a9da
martinjhnhadley <martinjohnhadley@gmail.com>
2016-12-21 14:15:27 +0000

    Added Emre basic visualisation

---
+++ 2016/Emre/data-processing.R
@@ -0,0 +1,69 @@
+library(readr)
+library(plyr)
+library(dplyr)
+library(lubridate)
+library(viridis)
+
+
+emre_data <- read_csv(read_csv("data/secret_events_url.csv") %>%
+                        select(events_url) %>%
+                        unlist(use.names = F))
+colnames(emre_data) <- tolower(gsub("[.]","",colnames(emre_data))) 
+colnames(emre_data) <- gsub(" ",".",colnames(emre_data))
+
+unique_species <- emre_data %>%
+  select(species) %>%
+  unique() %>%
+  unlist(use.names = F)
+
+seconds_to_period(emre_data$time)
+
+emre_data <- emre_data %>%
+  mutate(station_id = plyr::mapvalues(camera.id, from = unique_species, to = 1:length(unique_species))) %>%
+  mutate(species_id = plyr::mapvalues(species, from = unique_species, to = 1:length(unique_species))) %>%
+  mutate(station = gsub("_Camera_[A-Z]", "", camera.id)) %>%
+  mutate(time = seconds_to_period(time)) %>% # time in seconds rather than milliseconds
+  mutate(time = dmy_hms(paste(date,time))) %>%
+  mutate(radius = rep(5, nrow(emre_data))) %>%
+  mutate(color = plyr::mapvalues(species, from = unique_species, to = viridis::viridis(length(unique_species)))) %>% # chrome doesn't suppport 8 character hex colors (yet) but Firefox does
+  mutate(color = substr(color,1,nchar(color)-2)) %>%
+  filter(!species %in% c("Humans with or without domestic animals", "Humans", "Domestic animals", ""))
+
+
+emre_locations <- read_csv(read_csv("data/secret_locations.csv") %>%
+                        select(events_url) %>%
+                        unlist(use.names = F))
+emre_locations <- read_csv("http://users.ox.ac.uk/~oucs0030/temp/Locations-FOQMRuVAifoTmfWMDgsz6a.csv")
+colnames(emre_locations) <- tolower(gsub("[.]","",colnames(emre_locations))) 
+colnames(emre_locations) <- gsub(" ",".",colnames(emre_locations))
+
+emre_locations <- emre_locations %>%
+  mutate(id = station.no - 1) %>%
+  rename(x = x.coordinate,
+         y = y.coordinate)
+
+no_empties <- setdiff(1:max(emre_locations$id), emre_locations$id)
+empties <- data.frame(
+  id = no_empties,
+  x = rep(0, length(no_empties)),
+  y = rep(0, length(no_empties)),
+  station.no = no_empties + 1
+)
+
+all_locations <- full_join(emre_locations, empties) %>%
+  arrange(station.no)
+all_locations <- all_locations %>%
+  mutate(color = rep("pink", nrow(all_locations)),
+         radius = 10,
+         title = paste("Station", station.no))
+
+emre_data <- emre_data %>%
+  mutate(place_id = as.numeric(gsub("Station_", "", station)) - 1)
+
+
+
+
+
+
+
+
---
+++ 2016/Emre/visualisation.R
@@ -0,0 +1,64 @@
+## eventsObserver
+# if (!require('devtools')) install.packages('devtools')
+# devtools::install_github('ox-it/eventsObserveR')
+library(eventsObserveR)
+
+source("data-processing.R", local = TRUE)
+
+legend.df <- emre_data %>%
+  select(species, color) %>%
+  rename(description = species) %>%
+  unique()
+
+
+## ===== Tooltip
+
+emre_event_labeller <- function(
+  no..individuals,
+  species,
+  camera.id,
+  time,
+  no..males,
+  no..females,
+  no..unknown,
+  no..sa,
+  no..juv){
+  paste0(
+    no..individuals," ",species," by ", camera.id, " at ", time,
+    " ",
+    no..males, " males, ", no..females, " females, ", no..unknown, " unknown.",
+    " ",
+    no..sa, " small adults, ", no..juv, " small animals."
+  )
+}
+
+emre_data <- emre_data %>%
+  mutate(title = emre_event_labeller(
+    no..individuals,
+    species,
+    camera.id,
+    time,
+    no..males,
+    no..females,
+    no..unknown,
+    no..sa,
+    no..juv)
+  )
+
+## ==== Filter out missing dates
+
+emre_data <- emre_data %>%
+  filter(!is.na(time))
+
+## ==== Visualisation
+
+eventsObserveR(events = emre_data,
+               place.key = "station",
+               places = all_locations,
+               legend = legend.df,
+               legend.columns = 2,
+               size = list(
+                 view.width = 1040,
+                 view.height = 720,
+                 interface.width = 1200,
+                 interface.height = 900))
